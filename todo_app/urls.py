0f70c9454008ffce57265ecc3202d78cee338b78
Subarna Saikia <71511449+subarnasaikia@users.noreply.github.com>
2023-08-13 02:27:50 +0530

    updating with templets for fron end.

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    
+    path("", views.ListListView.as_view(), name="index"),
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
 ]
